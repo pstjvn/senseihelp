dda088eb87bee1f0b55406ebf7a36a8cc2571dde
Peter StJ <regardingscot@gmail.com>
2016-05-15 17:36:20 +0300

    Wrap functionlity into callable objects

--- js/inject.js
+++ js/inject.js
@@ -5,6 +5,20 @@
 goog.provide('inject');
 
 goog.require('help.component.IconContainer');
+goog.require('pstj.storage.Storage');
 
-var cnt = new help.component.IconContainer();
-cnt.render(document.body);
+goog.exportSymbol('closure.help.showIcon', function() {
+  var cnt = new help.component.IconContainer();
+  cnt.render(document.body);
+});
+
+goog.exportSymbol('closure.help.activateButtonIfNeeded', function() {
+  var storage = pstj.storage.Storage.getInstance();
+  var lastUserId = storage.get('lastUserId');
+  if (goog.isDefAndNotNull(lastUserId)) {
+    var userHasSeenIt = storage.get('_hasSeenIt_' + lastUserId);
+    if (!userHasSeenIt) {
+      goog.global['closure']['help']['showIcon']();
+    }
+  }
+});
